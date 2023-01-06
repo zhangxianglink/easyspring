551e250b5c4f8ec3941b2bf48c14abe92643f340
xiang z <zhangxianglink@163.com>
2023-01-06 11:20:32 +0800

    fea: aop数据准备

---
+++ src/test/java/org/easyspring/service/v5/PetStoreService5.java
@@ -0,0 +1,34 @@
+package org.easyspring.service.v5;
+
+import org.easyspring.beans.factory.annotation.Autowired;
+import org.easyspring.dao.v4.AccountDao;
+import org.easyspring.dao.v4.ItemDao;
+import org.easyspring.stereotype.Component;
+
+/**
+ * @author xiangzhang
+ * @since 2022-12-11 14:14
+ */
+@Component(value = "petStore")
+public class PetStoreService5 {
+
+    @Autowired
+    private AccountDao accountDao;
+    @Autowired
+    private ItemDao itemDao;
+
+    public PetStoreService5() {
+    }
+
+    public AccountDao getAccountDao() {
+        return accountDao;
+    }
+
+    public ItemDao getItemDao() {
+        return itemDao;
+    }
+
+    public void placeOrder() {
+        System.out.println("place order");
+    }
+}
